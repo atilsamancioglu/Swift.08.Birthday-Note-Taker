e31fabd4bb6f761cee2bd1282d4357060ea0cb9b
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-02-12 20:14:05 +0300

    final version

--- Birthday Note Taker/ViewController.swift
+++ Birthday Note Taker/ViewController.swift
@@ -9,17 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var birthdayText: UITextField!
+    @IBOutlet weak var storedNameLabel: UILabel!
+    @IBOutlet weak var storedDateLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedDate = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let newName = storedName as? String {
+            storedNameLabel.text = "Stored Name: \(newName)"
+        }
+        
+        if let newDate = storedDate as? String {
+            storedDateLabel.text = "Stored Date: \(newDate)"
+        }
+        
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        UserDefaults.standard.set(nameText.text, forKey: "name")
+        UserDefaults.standard.set(birthdayText.text, forKey: "birthday")
+        UserDefaults.standard.synchronize()
+        
+        storedNameLabel.text = "Stored Name: \(nameText.text!)"
+        storedDateLabel.text = "Stored Date: \(birthdayText.text!)"
+        
     }
 
+
+    @IBAction func deleteButtonClicked(_ sender: Any) {
+        
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedDate = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let _ = storedName as? String {
+
+            UserDefaults.standard.removeObject(forKey: "name")
+            UserDefaults.standard.synchronize()
+            storedNameLabel.text = "Stored Name: "
+        
+        }
+        
+        if let _ = storedDate as? String {
+           
+            UserDefaults.standard.removeObject(forKey: "birthday")
+            UserDefaults.standard.synchronize()
+            storedDateLabel.text = "Stored Date: "
+            
+        }
+        
+    }
 
 }
 
